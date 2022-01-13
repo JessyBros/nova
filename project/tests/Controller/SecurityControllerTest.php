bd3bcb3f45727e7c9980de567041e01c7b34f17b
SiProdZz <j.bros@hotmail.fr>
2022-01-13 20:22:51 +0100

    add test connexion without fixtures

--- project/tests/Controller/SecurityControllerTest.php
+++ project/tests/Controller/SecurityControllerTest.php
@@ -18,8 +18,8 @@ class SecurityControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h2', 'Inscription');
 
         $crawler = $client->submitForm('Inscription', [
-            'registration_form[username]' => 'Fabien',
-            'registration_form[email]' => 'emraifl@gmail.com',
+            'registration_form[username]' => 'Jessy',
+            'registration_form[email]' => 'j.bros@hotmail.fr',
             'registration_form[password][first]' => 'password',
             'registration_form[password][second]' => 'password',
         ]);
@@ -27,6 +27,25 @@ class SecurityControllerTest extends WebTestCase
         $crawler = $client->followRedirect();
 
         $this->assertSelectorTextContains('div', 'Inscription réussi');
+        $this->assertResponseIsSuccessful();
+        
+    }
+
+    public function testConnexion(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSelectorTextContains('h2', 'Connexion');
+
+        $crawler = $client->submitForm('Connexion', [
+            '_username' => 'j.bros@hotmail.fr',
+            '_password' => 'password',
+        ]);
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSelectorTextContains('a', 'Se déconnecter');
         $this->assertResponseIsSuccessful();
         
     }
